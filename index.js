e8a9a7a087d02697abc079f94a5071ab7c629052
Taariq Mullins <tjma2001@gmail.com>
2018-08-08 15:31:28 +0200

    refactor(server): beCORS

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ var config = parseServerConfig(__dirname);
 // Modify config as necessary before initializing parse server & dashboard
 
 var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/parse', new ParseServer(config.server));
 app.use('/parse-dashboard', ParseDashboard(config.dashboard, true));
