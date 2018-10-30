e7176d3e37e642749faa62f77d268518445bbf2d
Taariq Mullins <tjma2001@gmail.com>
2018-10-31 01:22:57 +0200

    refactor: index

--- index.js
+++ index.js
@@ -2,26 +2,27 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var ParseDashboard = require('parse-dashboard');
 var parseServerConfig = require('parse-server-azure-config');
-var url = require('url');
+var url = require('url')
+const { path } = require('fs')
 
-var config = parseServerConfig(__dirname);
+var config = parseServerConfig(__dirname)
 
 // Modify config as necessary before initializing parse server & dashboard
 
-var app = express();
+var app = express()
 
 // CORS please
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
-app.get('/ping', (req, res) => res.status(200).send('pong'));
-app.use('/public', express.static(__dirname + '/public'));
-app.use('/parse', new ParseServer(config.server));
-app.use('/parse-dashboard', ParseDashboard(config.dashboard, true));
+app.get('/ping', (req, res) => res.status(200).send('pong'))
+app.use('/public', express.static(path.join(__dirname, '/public')))
+app.use('/parse', new ParseServer(config.server))
+app.use('/parse-dashboard', ParseDashboard(config.dashboard, true))
 
 app.listen(process.env.PORT || url.parse(config.server.serverURL).port, function () {
-  console.log(`Parse Server running at ${config.server.serverURL}`);
-});
+  console.log(`Parse Server running at ${process.env.PORT || url.parse(config.server.serverURL).port}`)
+})
